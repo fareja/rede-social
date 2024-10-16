36b1986d4d97de4c2661803a7ea4c7e504ac773e
fareja <164102505+fareja@users.noreply.github.com>
2024-10-15 21:01:08 -0300

    const

--- infomacoesGlobais.js
+++ infomacoesGlobais.js
@@ -1,2 +1,6 @@
 const url ='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-console.log(url);
+
+async function visualizarinformacoesGlobais() {
+    const res =await fetch(url)
+    
+}
